0bb4c29628a853607e686edb0d0e8734a51074c4
Yury Alencar Lima <yuryalencar19@gmail.com>
2019-08-22 03:15:17 -0300

    ADDED: New Methods for TodoApp

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Grid from '../template/grid';
+import IconButton from '../template/iconButton';
 
 export default props => (
-    <div>
-        <h1>Form</h1>
+    <div role="form" className="todoForm">
+        <Grid cols="12 9 10">
+            <input id="description" className="form-control"
+                placeholder="Adicione uma Tarefa"
+                onChange={props.handleChange}
+                value={props.description}></input>
+        </Grid>
+
+        <Grid cols="12 3 2">
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+            <IconButton style='info' icon='search' onClick={props.handleSearch}></IconButton>
+            <IconButton style='default' icon='close' onClick={props.handleClear}></IconButton>
+        </Grid>
     </div>
 );
